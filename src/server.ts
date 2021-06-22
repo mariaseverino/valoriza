a6a6cea4e21f1c63d90dc4878461df222e06ca52
Maria Rita de Souza Severino <mariarsseverino@gmail.com>
2021-06-21 21:19:45 -0300

    :sparkles: Aula 01 – Liftoff

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.get("/test", (request, response) => {
+    return response.send("Olá NLW");
+});
+
+app.post("/test-post", (request, response) => {
+    return response.send("Olá NLW metodo post");
+});
+
 app.listen(3000, () => console.log("Server is running"));
