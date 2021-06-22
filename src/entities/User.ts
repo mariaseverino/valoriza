44e07a79ee47549e3f7b5b71a5b0083aaf191f01
Maria Rita de Souza Severino <mariarsseverino@gmail.com>
2021-06-22 15:43:24 -0300

    🗃️ configurações iniciais do banco de dados

---
+++ src/entities/User.ts
@@ -0,0 +1,6 @@
+import { Entity } from "typeorm";
+
+@Entity("Users")
+class User {}
+
+export { User };
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,8 @@
 import express from "express";
+import "reflect-metadata";
+
+import "./database";
 
 const app = express();
-
-app.get("/test", (request, response) => {
-    return response.send("Olá NLW");
-});
-
-app.post("/test-post", (request, response) => {
-    return response.send("Olá NLW metodo post");
-});
 
 app.listen(3000, () => console.log("Server is running"));
